adfe0f671d4ffad27e28437775d46ae4b420a880
Mpriyabrata <priyabrata.m1@gmail.com>
2021-05-22 20:46:11 +0530

    Jenkinsfile updated

--- env.groovy
+++ env.groovy
@@ -3,15 +3,8 @@ def now = new Date()
 def build_time = now.format("yyMMdd_HHmm", TimeZone.getTimeZone('PST'))
 def buildName = Jenkins.instance.getItemByFullName("OrgAuto/Test/main")
 env.buildnumber = buildName.getLastSuccessfulBuild().getNumber()
-// env.uploadSpec = """{
-//             "files": [
-//                 {
-//                     "pattern": "*.zip",
-//                     "target": "myrepo/${currentBuild.number}_${build_time}/${env.GIT_COMMIT}/",
-//                     "props": "type=zip;status=ready"
-//                 }
-//             ]
-//     }"""
+
+
 env.downloadSpec = """{
             "files": [
                 {
