fd55a456b717b9b02a7aacc922e54f9e8a600391
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-05-22 13:54:56 +0530

    download artifacts only

--- env.groovy
+++ env.groovy
@@ -3,6 +3,7 @@ def now = new Date()
 def build_time = now.format("yyMMdd_HHmm", TimeZone.getTimeZone('PST'))
 def buildName = Jenkins.instance.getItemByFullName("OrgAuto/Test/main")
 buildnumber = buildName.getLastSuccessfulBuild().getNumber()
+properties([parameters([string(defaultValue: ${buildnumber}, description: 'Buils number', name: 'Buld No: ', trim: false)])])
 // env.uploadSpec = """{
 //             "files": [
 //                 {
